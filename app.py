f6521c7502e6a4422b2ca93592b9db9571158636
omer <omerduru01@hotmail.com>
2025-06-01 13:48:50 +0300

    Su kalite CSV dosyası ve Streamlit app eklendi

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 st.set_page_config(page_title="Su Kalite Testi", layout="wide")
 
 # CSV dosya yolu (aynı klasörde su_kalite_standartlari.csv olmalı)
-CSV_FILE = "su_kalite_standartlari.csv"
+CSV_FILE = "su_kalite_standartlari.csv.txt"
 
 @st.cache_data
 def fetch_limits():
